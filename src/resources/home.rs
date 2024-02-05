90a5a9db46b7a6cf0df2e8762f3704995f4ceb11
Tiras Mwangi <tmwangi599@gmail.com>
2024-02-05 21:40:52 +0300

    home route

---
+++ src/resources/home.rs
@@ -0,0 +1,22 @@
+use askama::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse}
+};
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct HomeTemplate;
+
+pub async fn home() -> impl IntoResponse {
+    let template = HomeTemplate;
+
+    match template.render() {
+        Ok(html) => Html(html).into_response(),
+        Err(err) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Failed to render template. Error: {err}"),
+        )
+            .into_response(),
+    }
+}
