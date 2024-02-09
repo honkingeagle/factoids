a2e66f4be64f00048a3fd31b0f4f79da716e33cf
Tiras Mwangi <tmwangi599@gmail.com>
2024-02-09 13:39:16 +0300

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use factoids::AppState;
 use sqlx::PgPool;
+use std::{process, sync::Arc};
 
 #[shuttle_runtime::main]
-async fn main(
-    #[shuttle_shared_db::Postgres] _pool: PgPool
-) -> shuttle_axum::ShuttleAxum {
-    let router = factoids::create_router();
+async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
+    sqlx::migrate!().run(&pool).await.unwrap_or_else(|err| {
+        eprintln!("Unable to migrate sql files: {err}");
+        process::exit(1);
+    });
+
+    let state = Arc::new(AppState::new(pool));
+
+    let router = factoids::create_router(state);
 
     Ok(router.into())
 }
