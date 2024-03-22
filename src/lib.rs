739a5765b771a5b46d0a96d1b74a29e34084aa45
Tiras Mwangi <tmwangi599@gmail.com>
2024-03-22 21:03:45 +0300

    updated router paths

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 pub mod resources;
 
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 use sqlx::PgPool;
 use std::sync::Arc;
 use tower_http::services::ServeDir;
@@ -19,10 +22,15 @@ impl AppState {
 
 pub fn create_router(state: SharedState) -> Router {
     Router::new()
-        .route("/", get(resources::home).post(resources::create_slang_word))
+        .route("/", get(resources::home))
+        .route("/slangword/create", post(resources::create_slang_word))
+        .route("/slangword/store", get(resources::go_to_store_page))
+        .route("/slangword/update/:id", get(resources::go_to_update_page))
         .route(
             "/slangword/:id",
-            get(resources::get_slang_word).delete(resources::delete_slang_word),
+            get(resources::get_slang_word)
+                .put(resources::update_slang_word)
+                .delete(resources::delete_slang_word),
         )
         .nest_service("/public", ServeDir::new("public"))
         .with_state(state)
