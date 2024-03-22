adee6d79eea0e1d662f6fa24c3de8f568613bb17
Tiras Mwangi <tmwangi599@gmail.com>
2024-03-22 21:02:57 +0300

    root module for resources

--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -1,9 +1,23 @@
 pub mod delete;
 pub mod get_one;
 pub mod home;
-pub mod post;
+pub mod store;
+pub mod store_page;
+pub mod update;
+pub mod update_page;
 
-pub use delete::delete_slang_word;
-pub use get_one::get_slang_word;
 pub use home::home;
-pub use post::create_slang_word;
+pub use get_one::get_slang_word;
+pub use store::create_slang_word;
+pub use update::update_slang_word;
+pub use store_page::go_to_store_page;
+pub use update_page::go_to_update_page;
+pub use delete::delete_slang_word;
+
+#[derive(sqlx::FromRow, serde::Deserialize)]
+pub struct SlangWord {
+    pub id: i32,
+    pub word: String,
+    pub synonym: String,
+    pub description: String,
+}
