ba4a30b94192914d14232906b4e95eda6ef967b3
Tiras Mwangi <tmwangi599@gmail.com>
2024-03-22 21:01:11 +0300

    shows individual slangwords

--- src/resources/get_one.rs
+++ src/resources/get_one.rs
@@ -1,22 +1,55 @@
+use super::SlangWord;
+use crate::SharedState;
 use askama::Template;
 use axum::{
-    extract::Path,
+    extract::{Path, State},
     http::StatusCode,
     response::{Html, IntoResponse},
 };
 
 #[derive(Template)]
-#[template(path = "slangword.html")]
-struct SlangWordTemplate;
+#[template(path = "slangwords/show.html")]
+struct SlangWordTemplate {
+    pub slang_word: SlangWord,
+}
 
-pub async fn get_slang_word(Path(_id): Path<i32>) -> impl IntoResponse {
-    let template = SlangWordTemplate;
+impl SlangWordTemplate {
+    pub fn new(slang_word: SlangWord) -> Self {
+        Self { slang_word }
+    }
+}
 
-    match template.render() {
-        Ok(html) => Html(html).into_response(),
-        Err(err) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Failed to render template. Error: {err}"),
+pub async fn get_slang_word(
+    State(state): State<SharedState>,
+    Path(id): Path<i32>,
+) -> impl IntoResponse {
+    let query = sqlx::query_as::<_, SlangWord>(
+        r#"
+            select * 
+            from slangwords 
+            where id = $1
+        "#,
+    )
+    .bind(id)
+    .fetch_one(&state.pool)
+    .await;
+
+    match query {
+        Ok(slang_word) => {
+            let template = SlangWordTemplate::new(slang_word);
+
+            match template.render() {
+                Ok(html) => Html(html).into_response(),
+                Err(_) => (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Failed to render template!"),
+                )
+                    .into_response(),
+            }
+        }
+        Err(_) => (
+            StatusCode::NOT_FOUND,
+            "No slangword with that id is available".to_string(),
         )
             .into_response(),
     }
