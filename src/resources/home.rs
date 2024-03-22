9f1c6a15b347551392a390c416a389f455e5c0e7
Tiras Mwangi <tmwangi599@gmail.com>
2024-03-22 21:00:28 +0300

    homepage

--- src/resources/home.rs
+++ src/resources/home.rs
@@ -1,21 +1,74 @@
+use super::SlangWord;
+use crate::SharedState;
 use askama::Template;
 use axum::{
+    extract::{Query, State},
     http::StatusCode,
     response::{Html, IntoResponse},
 };
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Pagination {
+    pub page: i32,
+    pub limit: i32,
+}
+
+impl Default for Pagination {
+    fn default() -> Pagination {
+        Pagination { page: 1, limit: 5 }
+    }
+}
 
 #[derive(Template)]
-#[template(path = "index.html")]
-struct HomeTemplate;
+#[template(path = "slangwords/index.html")]
+struct HomeTemplate {
+    slang_words: Vec<SlangWord>,
+}
 
-pub async fn home() -> impl IntoResponse {
-    let template = HomeTemplate;
+impl HomeTemplate {
+    pub fn new(slang_words: Vec<SlangWord>) -> HomeTemplate {
+        HomeTemplate { slang_words }
+    }
+}
 
-    match template.render() {
-        Ok(html) => Html(html).into_response(),
-        Err(err) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Failed to render template. Error: {err}"),
+pub async fn home(
+    State(state): State<SharedState>,
+    paginator: Option<Query<Pagination>>,
+) -> impl IntoResponse {
+    let Query(paginator) = paginator.unwrap_or_default();
+
+    let offset = (paginator.page - 1) * paginator.limit;
+
+    let query = sqlx::query_as::<_, SlangWord>(
+        r#"
+        select * 
+        from slangwords
+        order by word
+        limit $1 offset $2
+        "#,
+    )
+    .bind(paginator.limit)
+    .bind(offset)
+    .fetch_all(&state.pool)
+    .await;
+
+    match query {
+        Ok(slang_words) => {
+            let template = HomeTemplate::new(slang_words);
+
+            match template.render() {
+                Ok(html) => Html(html).into_response(),
+                Err(_) => (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Failed to render template!"),
+                )
+                    .into_response(),
+            }
+        }
+        Err(_) => (
+            StatusCode::NOT_FOUND,
+            format!("No other slangwords available!"),
         )
             .into_response(),
     }
