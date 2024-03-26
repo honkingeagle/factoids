ce2f4da1b064de5a5f7f3faa61f94fe3c0581413
Tiras Mwangi <tmwangi599@gmail.com>
2024-03-26 19:40:39 +0300

    html page + routes for updating a slangword resource

---
+++ src/resources/update.rs
@@ -0,0 +1,45 @@
+use crate::SharedState;
+use axum::{
+    extract::{Form, State, Path},
+    response::{IntoResponse, Redirect},
+    http::StatusCode,
+};
+
+#[derive(sqlx::FromRow, serde::Deserialize)]
+pub struct SlangWord {
+    pub word: String,
+    pub synonym: String,
+    pub description: String,
+}
+
+pub async fn update_slang_word(
+    State(state): State<SharedState>,
+    Path(id): Path<i32>,
+    Form(slangword): Form<SlangWord>,
+) -> impl IntoResponse {
+    let query = sqlx::query(
+        r#"
+            update slangwords 
+            set word = $1, synonym = $2, description = $3
+            where id = $4
+        "#,
+    )
+    .bind(slangword.word)
+    .bind(slangword.synonym)
+    .bind(slangword.description)
+    .bind(id)
+    .execute(&state.pool)
+    .await;
+
+    match query {
+        Ok(_) => {
+            let uri = format!("/slangword/{}", id);
+            
+            Redirect::to(&uri).into_response()
+        }
+        Err(err) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Err {err}")
+        ).into_response(),
+    }
+}
---
+++ src/resources/update_page.rs
@@ -0,0 +1,55 @@
+use super::SlangWord;
+use crate::SharedState;
+use askama::Template;
+use axum::{
+    extract::{ Path, State,},
+    http::StatusCode,
+    response::{ Html, IntoResponse },
+};
+
+#[derive(Template)]
+#[template(path = "slangwords/update.html")]
+struct UpdateTemplate {
+    slangword: SlangWord
+}
+
+impl UpdateTemplate {
+    pub fn new(slangword: SlangWord) -> Self {
+        Self { slangword }
+    }
+}
+
+pub async fn go_to_update_page(
+    State(state): State<SharedState>,
+    Path(id): Path<i32>,
+) -> impl IntoResponse {
+    let query = sqlx::query_as::<_, SlangWord>(
+        r#"
+            select * 
+            from slangwords 
+            where id = $1
+        "#,
+    )
+    .bind(id)
+    .fetch_one(&state.pool)
+    .await;
+
+    match query {
+        Ok(slang_word) => {
+            let template = UpdateTemplate::new(slang_word);
+        
+            match template.render() {
+                Ok(html) => Html(html).into_response(),
+                Err(_) => (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Failed to render template!"),
+                )
+                    .into_response(),
+            }
+        }
+        Err(_) => (
+            StatusCode::NOT_FOUND
+        ).into_response(),
+    }
+
+}
